fa369bfe89f56d7649fe32d41e16575bbea7ce21
Crothos <s.g.totten@gmail.com>
2023-02-07 20:44:09 -0600

    tweaked index.js models

--- models/index.js
+++ models/index.js
@@ -11,23 +11,23 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+  onDelete: 'SET NULL',
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
-    unique: false
-  },
-  as: 'tag_product1'
+    unique: false,
+    foreignKey: 'product_id',
+  }
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
-    uniue: false
-  },
-  as: 'tag_product2'
+    unique: false,
+    foreignKey: 'tag_id',
+  }
 });
 module.exports = {
   Product,
